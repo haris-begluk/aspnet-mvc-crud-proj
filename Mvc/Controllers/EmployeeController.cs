fd8bbe00381226baa47ea966ed4fbf85427680ba
HarisBegluk <haris.begluk.webprogramming@outlook.com>
2018-02-05 19:27:46 +0100

    CRUD MVC Application with Web API example

---
+++ Mvc/Controllers/EmployeeController.cs
@@ -0,0 +1,65 @@
+﻿using Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<MvcEmployeeModel> empList;
+            HttpResponseMessage response =  GlobalVariables.WebApiClient.GetAsync("Employee").Result;
+            empList = response.Content.ReadAsAsync<IEnumerable<MvcEmployeeModel>>().Result; 
+
+            return View(empList);
+        }
+
+
+
+        public ActionResult AddOrEdit(int id = 0)
+        {
+           
+            if(id == 0)
+                return View(new MvcEmployeeModel()); 
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employee/" + id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<MvcEmployeeModel>().Result);
+                
+            }
+        }  
+
+
+
+
+        [HttpPost]
+        public ActionResult AddOrEdit( MvcEmployeeModel emp)
+        {
+            if (emp.EmployeeID == 0)
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Employee", emp).Result;
+                TempData["SuccessMessage"] = "Saved Successfully";
+            }else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync("Employee/" + emp.EmployeeID,emp ).Result;
+                TempData["SuccessMessage"] = "Updated Successfully";
+            }
+            return RedirectToAction("Index");
+        } 
+
+
+        public ActionResult Delete( int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Employee/" + id.ToString()).Result;
+            TempData["SuccessMessage"] = "Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
